07b95dde9024e69c27071b4471f6895bb9fbff78
chumplizz <48303627+elizabethranta@users.noreply.github.com>
2023-04-19 22:14:47 -0700

    Http function called API!

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (context, req) {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."; 
+
+        //httpGet("https://official-joke-api.appspot.com/random_joke")
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: httpGet("https://official-joke-api.appspot.com/random_joke")
     };
+
+    function httpGet(theUrl)
+    {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+    }
 }
